89a38990ae545444af9433a29254f24e96f2491b
Jochen Fähnlein <jochen.faehnlein@ff-ipsheim.de>
2022-08-19 22:22:27 +0200

    Multithreading

--- generate.py
+++ generate.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
+import logging
 import os
 import subprocess
 from lxml import etree
 from datetime import datetime
+import time
+import concurrent.futures
 
 
-printmaps = "bin/printmaps"
-
+printmaps = os.path.abspath('./bin/printmaps')
 
 class MapDefinition:
     def __init__(self, path):
@@ -15,36 +17,42 @@ class MapDefinition:
             raise "path %s is not a directory" % path
         
         self.path = os.path.abspath(path)
+        self.name = os.path.basename(path)
+        
+    
+    def __execute_command(action):
+        
+        logging.info(f'{self.name}: call \'{action}\'')
+        
+        #result = subprocess.call([printmaps, action], shell=True)
+        result = subprocess.run([printmaps, action], capture_output=True, shell=True, text=True, cwd=self.path)
+        
+        return result
 
 
     def __renew_map_id(self):
         filename = "map.id"
         abs_filename = os.path.join(self.path, filename)
-        command = "%s %s", printmaps, "create"
-        
+                
         if os.path.exists(abs_filename):
-            os.delete(abs_filename)
+            os.remove(abs_filename)
         
-        print("create %s" % filename)
-        os.exec(command)
+        logging.info(f'{self.name}: create {filename}')
+        
+        self.__execute_command('create')
     
     
     def __try_to_update(self):
-        command = '{0} {1}'.format(printmaps, 'update')
-        
-        # returned_value = subprocess.call(command, shell=True)  # returns the exit code in unix
-        returned_output = subprocess.check_output(command) # returns output as byte string
+        # returned_output = subprocess.check_output(command) # returns output as byte string
+        returned_output = self.__execute_command('update')
         
         # using decode() function to convert byte string to string
-        parsed_output = returned_output.decode('utf-8')
+        #parsed_output = returned_output.decode('utf-8')
         
-        
-        print('Result:', parsed_output)
-        
-        pass
+        logging.info(f'{self.name}: result: {returned_output}')
     
     
-    def update_legend(self):
+    def __update_legend(self):
         legend_file = 'legend.svg'
         legend_file_as_path = 'legend-path.svg'
         
@@ -57,43 +65,63 @@ class MapDefinition:
             tree = etree.parse(source_file)
         
         node = tree.find('//{0}text[@id="Date"]/{0}tspan'.format(svg_ns))
-        print('n=', node)
-            
+        
         currDate = datetime.now()
         formatted_date = currDate.strftime('%d.%m.%Y')
         node.text = formatted_date
         
-        print('update {0} with current date'.format(legend_file))
+        logging.info(f'{self.name}: update {legend_file} with current date')
         
         with open(abs_legend_file, 'wb') as target:
             tree.write(target, encoding="utf-8", xml_declaration=True, pretty_print=True)
         
-        print('svg: convert text to path')
+        logging.info(f'{self.name}: convert legend files\' text to path')
+        
         inkscape_command = 'inkscape "{0}" --export-text-to-path --export-plain-svg -o "{1}"'.format(abs_legend_file, abs_legend_file_as_path)
         returned_output = subprocess.call(inkscape_command, shell = True)
-        print('inkscape returned {0}'.format(returned_output))
-        
-        
-    
+
 
     def generate(self):
-        command = "%s %s", printmaps, "update"
+        self.__renew_map_id()
+        self.__update_legend()
         
-        print("generate map %s ..." % self.path.name)
+        logging.info(f'{self.name}: generate map ...')
+        
+        # arguments = ['update', 'upload'] # , 'order', 'download', 'unzip'
+        # for argument in arguments:
+            
+        #     returned_output = self.__execute_command(argument)
+        
+        #     # using decode() function to convert byte string to string
+        #     parsed_output = returned_output.decode('utf-8')
+        
+        time.sleep(5)
+        
         
     
     def __str__(self):
         return f'Map(\'{self.path}\')'
         
 
-folders_to_ignore = ['bin', 'shared']
-current_dir_path = os.path.dirname(os.path.realpath(__file__))
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+    logging.getLogger().setLevel(logging.DEBUG)
+    
+    folders_to_ignore = ['bin', 'shared']
+    current_dir_path = os.path.dirname(os.path.realpath(__file__))
 
-# get all top level directories without folders to ignore and hidden '.*' folders
-toplevel_directories = [os.path.join(current_dir_path, p) for p in next(os.walk(current_dir_path))[1] if p not in folders_to_ignore and p[0] != '.' and p[0] != '_']
+    # get all top level directories without folders to ignore and hidden '.*' folders
+    toplevel_directories = [os.path.join(current_dir_path, p) for p in next(os.walk(current_dir_path))[1] if p not in folders_to_ignore and p[0] != '.' and p[0] != '_']
 
-maps = [MapDefinition(d) for d in toplevel_directories]
+    maps = [MapDefinition(d) for d in toplevel_directories]
 
-for map in maps:
-    print('update map', map.path)
-    map.update_legend()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=len(maps)) as executor:
+        for map in maps:
+            executor.submit(map.generate)
+    
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for map in maps:
+            executor.submit(map.generate)
+            
+    logging.info("Finished.")
