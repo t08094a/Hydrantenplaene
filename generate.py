1cc1a8e2e9bbbeb4258e53f937c7cdd1a9313767
Jochen Fähnlein <jochen.faehnlein@ff-ipsheim.de>
2023-03-01 20:41:40 +0100

    Generierung vervollständigt

--- generate.py
+++ generate.py
@@ -1,15 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import logging
 import os
 import subprocess
+import time
+
+from re import search
 from lxml import etree
 from datetime import datetime
-import time
-import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor, wait, ALL_COMPLETED
 
+print_maps_path = os.path.abspath('./bin/printmaps')
 
-printmaps = os.path.abspath('./bin/printmaps')
 
 class MapDefinition:
     def __init__(self, path):
@@ -18,17 +20,14 @@ class MapDefinition:
         
         self.path = os.path.abspath(path)
         self.name = os.path.basename(path)
-        
     
-    def __execute_command(action):
-        
+    def __execute_command(self, action: str) -> (int, str):
         logging.info(f'{self.name}: call \'{action}\'')
         
-        #result = subprocess.call([printmaps, action], shell=True)
-        result = subprocess.run([printmaps, action], capture_output=True, shell=True, text=True, cwd=self.path)
-        
-        return result
+        result = subprocess.run([print_maps_path, action], capture_output=True, text=True, cwd=self.path)
+        output: str = result.stdout
 
+        return result.returncode, output
 
     def __renew_map_id(self):
         filename = "map.id"
@@ -39,19 +38,16 @@ class MapDefinition:
         
         logging.info(f'{self.name}: create {filename}')
         
-        self.__execute_command('create')
-    
-    
+        return_code, _ = self.__execute_command('create')
+
+        if return_code != 0:
+            logging.error(f'{self.name}: unable to create {filename}')
+
     def __try_to_update(self):
-        # returned_output = subprocess.check_output(command) # returns output as byte string
         returned_output = self.__execute_command('update')
         
-        # using decode() function to convert byte string to string
-        #parsed_output = returned_output.decode('utf-8')
-        
         logging.info(f'{self.name}: result: {returned_output}')
-    
-    
+
     def __update_legend(self):
         legend_file = 'legend.svg'
         legend_file_as_path = 'legend-path.svg'
@@ -66,8 +62,8 @@ class MapDefinition:
         
         node = tree.find('//{0}text[@id="Date"]/{0}tspan'.format(svg_ns))
         
-        currDate = datetime.now()
-        formatted_date = currDate.strftime('%d.%m.%Y')
+        current_date = datetime.now()
+        formatted_date = current_date.strftime('%d.%m.%Y')
         node.text = formatted_date
         
         logging.info(f'{self.name}: update {legend_file} with current date')
@@ -78,50 +74,75 @@ class MapDefinition:
         logging.info(f'{self.name}: convert legend files\' text to path')
         
         inkscape_command = 'inkscape "{0}" --export-text-to-path --export-plain-svg -o "{1}"'.format(abs_legend_file, abs_legend_file_as_path)
-        returned_output = subprocess.call(inkscape_command, shell = True)
+        returned_output = subprocess.call(inkscape_command, shell=True)
 
+    def __wait_until_map_build_is_finished(self):
+        finished_successful = False
+        timeout = time.time() + 60 * 10  # 10 minutes timeout
+
+        while not finished_successful and time.time() < timeout:
+            return_code, output = self.__execute_command('state')
+
+            if return_code == 0:
+                status_code_match = search(r'received status = \'(\d+) \w+\'', output)
+                status_value = int(status_code_match.group(1) if status_code_match is not None else '-1')
+
+                if status_value != 200:
+                    time.sleep(5)
+                    continue
+
+                status_match = search(r'attend status of \'(\w+)\'', output)
+                status = status_match.group(1) if status_match is not None else '<Not found>'
+                if status == 'MapBuildSuccessful':
+                    finished_successful = True
+                    logging.info(f'{self.name}: map build finished successful')
+                else:
+                    logging.info(f'{self.name}: map build not finished yet')
+                    time.sleep(30)
+
+        if not finished_successful:
+            logging.error(f'{self.name}: map build not finished and ran into timeout')
 
     def generate(self):
         self.__renew_map_id()
         self.__update_legend()
         
         logging.info(f'{self.name}: generate map ...')
-        
-        # arguments = ['update', 'upload'] # , 'order', 'download', 'unzip'
-        # for argument in arguments:
-            
-        #     returned_output = self.__execute_command(argument)
-        
-        #     # using decode() function to convert byte string to string
-        #     parsed_output = returned_output.decode('utf-8')
+
+        arguments = ['update', 'upload', 'order', 'download', 'unzip']
+        for argument in arguments:
+            return_code, output = self.__execute_command(argument)
+
+            if return_code != 0:
+                logging.error(f'{self.name}: action "{argument}" was not successful:\n{output}')
+
+            if argument == 'order':
+                self.__wait_until_map_build_is_finished()
+
+            time.sleep(5)
         
         time.sleep(5)
-        
-        
     
     def __str__(self):
         return f'Map(\'{self.path}\')'
         
 
 if __name__ == "__main__":
-    format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+    log_format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=log_format, level=logging.INFO, datefmt="%H:%M:%S")
     logging.getLogger().setLevel(logging.DEBUG)
     
     folders_to_ignore = ['bin', 'shared']
     current_dir_path = os.path.dirname(os.path.realpath(__file__))
 
     # get all top level directories without folders to ignore and hidden '.*' folders
-    toplevel_directories = [os.path.join(current_dir_path, p) for p in next(os.walk(current_dir_path))[1] if p not in folders_to_ignore and p[0] != '.' and p[0] != '_']
+    toplevel_directories = [os.path.join(current_dir_path, p) for p in next(os.walk(current_dir_path))[1] if p not in
+                            folders_to_ignore and p[0] != '.' and p[0] != '_']
 
     maps = [MapDefinition(d) for d in toplevel_directories]
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=len(maps)) as executor:
-        for map in maps:
-            executor.submit(map.generate)
-    
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        for map in maps:
-            executor.submit(map.generate)
-            
+    with ThreadPoolExecutor(max_workers=len(maps)) as executor:
+        futures = [executor.submit(current_map.generate) for current_map in maps]
+        wait(futures, timeout=60*10, return_when=ALL_COMPLETED)  # ALL_COMPLETED is actually the default
+
     logging.info("Finished.")
